24646a992155d779c3e94ad0adb456cec71942ed
Sonasunils <sona2002sunil@gmail.com>
2024-05-22 16:16:27 +0530

    Printing variables -Printing values along with statements.

---
+++ Exercise_5.py
@@ -0,0 +1,23 @@
+#variables and printing
+
+#declearing values
+
+my_name = "Sona"
+my_age = 21
+my_height =74
+my_weight= 62
+my_eyes = "Blue"
+my_teeth = "white"
+my_hair = "brown"
+
+
+#printing the decleard values
+print(f"Let's talk anout {my_name}.")
+print(f" He's {my_height} inches tall.")
+print(f" He's {my_weight} pounds heavy.")
+print(f" Actually that's not too heavy")
+print(f" He's got{my_eyes} eyes and {my_hair} hair.")
+print(f"His teeth are usually {my_teeth} depending on the coffee")
+total = my_age + my_height + my_weight
+
+print(f"If I add {my_age}, {my_height},and {my_weight} I get {total}")
