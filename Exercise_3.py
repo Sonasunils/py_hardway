a1496f538b5e5630d658e5c6571db828a3352324
Sonasunils <sona2002sunil@gmail.com>
2024-05-22 13:24:25 +0530

    Numbers and maths
    This program deals with maths opertion

---
+++ Exercise_3.py
@@ -0,0 +1,15 @@
+#Number and math
+
+
+print("I will count chickens:")
+print("Hens", 25 + 30 / 6)
+print("Rooster", 100 - 25 * 3 % 4)
+print("Now it's counting the eggs")
+print(3 + 3 + 7 + 3 % 6 + 8 - 3 - 5 / 9 + 7)
+print("Is it true that 3 + 2 < -7?")                  #just print
+print(3 + 2 < 5 - 7)
+print("What is 3 + 2?", 3 + 2)                        #add and print
+print("What is 1 - 2?", 1 - 2)                        #subtact and print
+print("Is it greater?", 5 > -2)                       #check and print whether true or false.
+print("Is it greater or equal?", 5 >= -2)
+print("Is it less or equal?", 5 <= -2)
