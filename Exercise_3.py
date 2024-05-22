a2c7dcc0829e31ce11a9c3a917d00a448f730204
Sonasunils <sona2002sunil@gmail.com>
2024-05-22 13:28:05 +0530

    Numbers and math -This program deals with arithmatic and logic operations.

--- Exercise_3.py
+++ Exercise_3.py
@@ -9,7 +9,7 @@ print(3 + 3 + 7 + 3 % 6 + 8 - 3 - 5 / 9 + 7)
 print("Is it true that 3 + 2 < -7?")                  #just print
 print(3 + 2 < 5 - 7)
 print("What is 3 + 2?", 3 + 2)                        #add and print
-print("What is 1 - 2?", 1 - 2)                        #subtact and print
+print("What is 1 - 2?", 1 - 2)                        #subtract and print
 print("Is it greater?", 5 > -2)                       #check and print whether true or false.
 print("Is it greater or equal?", 5 >= -2)
 print("Is it less or equal?", 5 <= -2)
