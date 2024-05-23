42eba0a2b28a8055a18965790ed80bf6a1fc4758
Sonasunils <sona2002sunil@gmail.com>
2024-05-23 14:19:08 +0530

    Parameters, unpacking,variables. -The argv variable is a list that holds the command-line arguments passed to the script when it is executed.

---
+++ Exercise_13.py
@@ -0,0 +1,12 @@
+#Parameters,Unpacking,variables
+
+
+#import argv
+from sys import argv
+#the argument variable holds the argument we want to pass in out script 
+script, first, second, third = argv
+
+print("The script is called:", script)
+print("Your first variable is:", first)
+print("Your second variable is:", second)
+print("Your third variable is:", third)
