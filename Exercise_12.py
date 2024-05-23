154ee666e274290cbc2a81342e0b50bdc86d6b5a
Sonasunils <sona2002sunil@gmail.com>
2024-05-23 12:39:27 +0530

    Prompting people -prompting the user for input within the input() function.

---
+++ Exercise_12.py
@@ -0,0 +1,8 @@
+#Promoting people
+
+#use input function
+age = input("How old are you? ")
+height = input("How tall are you? ")
+weight = input("How much do you weigh? ")
+
+print(f"So, you're {age} old, {height} tall and {weight} heavy.")
