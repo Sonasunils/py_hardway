ae846a17c863be653817c1383a125d604886cede
Sonasunils <sona2002sunil@gmail.com>
2024-06-03 11:45:36 +0530

    This program perform encording.

--- Exercise_23.py
+++ Exercise_23.py
@@ -1,13 +1,14 @@
+#import modules
 import sys
 script, input_encoding, error = sys.argv
 
-
+#define funtion
 def main(language_file, encoding, errors):
     line = language_file.readline()
     if line:
         print_line(line, encoding, errors)
         return main(language_file, encoding, errors)
-
+#define function
 def print_line(line, encoding, errors):
     next_lang = line.strip()
     raw_bytes = next_lang.encode(encoding, errors=errors)
